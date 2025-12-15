4c53a17b6d41a5a39556a892582172511755b6b8
aleksandr <heyqa.404@gmail.com>
2025-12-15 11:01:20 -0800

    Начал писать первый тест

--- src/test/java/iteration_1/CreateUserTest.java
+++ src/test/java/iteration_1/CreateUserTest.java
@@ -10,16 +10,29 @@ import specs.RequestsSpecs;
 import specs.ResponseSpecs;
 
 public class CreateUserTest extends BaseTest {
+
+    // ГЕНЕРАЦИЯ ТЕСТОВЫХ ДАННЫх
     @Test
     public void adminCanCreateUserWithCorrectData() {
         CreateUserRequest createUserRequest = CreateUserRequest.builder()
-                .username(RandomData.getUsername())
-                .password(RandomData.getPassword())
-                .role(UserRole.USER.toString())
+                .username(RandomData.getUsername())  // Генерация username
+                .password(RandomData.getPassword())  // Генерация password
+                .role(UserRole.USER.toString()) // Установка роли
                 .build();
 
-        CreateUserResponse createUserResponse = new AdminCreateUserRequester(RequestsSpecs.adminSpec(),
-                ResponseSpecs.entityWasCreated())
+        // Сборка объекта
+        /*
+        {
+            "username": "AbCdEfGhIj",
+            "password": "ABCdefgh123$",
+            "role": "USER"
+        }
+         */
+
+        // СОЗДАНИЕ ПОЛЬЗОВАТЕЛЯ (Админом)
+        CreateUserResponse createUserResponse = new AdminCreateUserRequester(
+                RequestsSpecs.adminSpec(), // Настройка запроса - передаем спецификацию админа
+                ResponseSpecs.entityWasCreated()) // Настрока ожидаемого ответа (201 статус)
                 .post(createUserRequest).extract().as(CreateUserResponse.class);
 
         softly.assertThat(createUserRequest.getUsername()).isEqualTo(createUserResponse.getUsername());
--- src/test/java/iteration_2/UserDepositTest.java
+++ src/test/java/iteration_2/UserDepositTest.java
@@ -1,5 +1,41 @@
 package iteration_2;
 
-public class UserDepositTest {
+import generators.RandomData;
+import iteration_1.CreateUserTest;
+import models.CreateUserRequest;
+import models.CreateUserResponse;
+import models.UserRole;
+import org.junit.jupiter.api.Test;
+import requests.AdminCreateUserRequester;
+import specs.RequestsSpecs;
+import specs.ResponseSpecs;
 
+public class UserDepositTest extends BaseTest {
+
+    @Test
+    public void userDepositToAccountTest() {
+        // Генерация тестовых данных, вывел для наглядности перед созданием объекта
+        String username = RandomData.getUsername();
+        String password = RandomData.getPassword();
+
+        // Создание запроса(объекта) на создание юзера
+        CreateUserRequest userRequest = CreateUserRequest.builder()
+                .username(username) // рандомный username
+                .password(password) // рандомный password
+                .role(UserRole.USER.toString()) // роль обычного юзера, приведенная к строке
+                .build();
+
+        // Создается реквестер(объект для выполнения запроса) для админа
+        // Он знает:
+        // - куда отправлять запрос (спецификация админа)
+        // - какой ответ ожидать (спецификация "сущность создана" - статус 201)
+        AdminCreateUserRequester adminRequester = new AdminCreateUserRequester(
+                RequestsSpecs.adminSpec(), // спецификация админа
+                ResponseSpecs.entityWasCreated() // ожидание статуса 201 Created
+        );
+
+        CreateUserResponse userResponse = adminRequester.post(userRequest)
+                .extract()
+                .as(CreateUserResponse.class);
+    }
 }
